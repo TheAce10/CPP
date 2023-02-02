0d8fcc257e704d8bbd1eb00b9ecdf483e62d6956
TheAce10 <krapahbless@gmail.com>
2023-02-02 03:33:52 +0000

    01/02/2023

---
+++ D1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" <<'\n';
+    cout << "My first Cpp code";
+    
+    //Omitting namespace std
+    std::cout << "\nomitted namespace";
+
+    /*The newline character (\n) is called an escape sequence,
+     and it forces the cursor to change its position to the 
+    beginning of the next line on the screen. 
+    This results in a new line. */
+    cout << "\nHello World!\n";
+
+
+    //For new lines endl manipulator can be used
+    cout << "Hello World!" << endl;
+
+    /* In C++, there are different types of variables (defined with different keywords), for example:
+
+    int - stores integers (whole numbers), without decimals, such as 123 or -123
+    double - stores floating point numbers, with decimals, such as 19.99 or -19.99
+    char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+    string - stores text, such as "Hello World". String values are surrounded by double quotes
+    bool - stores values with two states: true or false*/
+
+    int myNum = 5;               // Integer (whole number without decimals)
+    double myFloatNum = 5.99;    // Floating point number (with decimals)
+    char myLetter = 'D';         // Character
+    string myText = "Hello";     // String (text)
+    bool myBoolean = true;       // Boolean (true or false)
+
+    //Display Variables
+    int myAge = 25;
+    cout << "I am " << myAge << " years old.";
+
+    //Declare Many Variables
+    int x = 5, y = 6, z = 50;
+    cout << x + y + z;
+
+    /*The general rules for naming variables are:
+
+    Names can contain letters, digits and underscores
+    Names must begin with a letter or an underscore (_)
+    Names are case sensitive (myVar and myvar are different variables)
+    Names cannot contain whitespaces or special characters like !, #, %, etc.
+    Reserved words (like C++ keywords, such as int) cannot be used as names*/
+    
+    /*Constants
+    When you do not want others (or yourself) to override 
+    existing variable values, use the const keyword 
+    (this will declare the variable as "constant", which means
+     unchangeable and read-only):*/
+
+     const int birthday = 101103;
+    //  birthday = 10; // error: assignment of read-only variable 'myNum'
+
+    //User Input
+    int xin;
+    cout << "Enter a number";
+    cin >> xin ;
+
+    //Exp
+    cout << xin ;
+
+    /*Basic Data types
+    Data type      Size        Description
+    boolean ----- 1 byte ---- stores true or false
+    char --------- 1 byte --- stores a single character\letter\number or ASCII
+    int ---------- 2\4 bytes - stores whole numbers without decimal points
+    float --------- 4 bytes --- stores fractional numbers upto 7 d.p.
+    double -------- 4 bytes --- stores fractional numbers upto 15 d.p.*/
+
+    //A floating point number can also be a scientific number with an "e" to indicate the power of 10:
+    float ff = 35e3;
+    double dd = 12E4;
+    cout << ff <<'\n' << dd;
+
+
+
+    return 0;
+}
