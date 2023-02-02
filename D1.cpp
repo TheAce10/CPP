ba8dd90850ba20914e464776798af5f7a4651e27
TheAce10 <krapahbless@gmail.com>
2023-02-02 00:23:24 +0000

    2

--- D1.cpp
+++ D1.cpp
@@ -8,6 +8,13 @@ int main() {
     //Omitting namespace std
     std::cout << "\nomitted namespace";
 
+    /*The newline character (\n) is called an escape sequence,
+     and it forces the cursor to change its position to the 
+    beginning of the next line on the screen. 
+    This results in a new line. */
+    cout << "\nHello World!\n";
+
+
     
 
 
