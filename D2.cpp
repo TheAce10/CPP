0445fb6c382602b05434c17171b451e5e803fcd3
TheAce10 <krapahbless@gmail.com>
2023-02-03 00:30:47 +0000

    7

--- D2.cpp
+++ D2.cpp
@@ -40,4 +40,14 @@ int main() {
     int sum2 = sum1 + 250;      // 400 (150 + 250)
     int sum3 = sum2 + sum2;     // 800 (400 + 400)
 
- 
+    /*
+    +	Addition	    Adds together two values	x + y	
+    -	Subtraction	    Subtracts one value from another	x - y	
+    *	Multiplication	Multiplies two values	x * y	
+    /	Division	    Divides one value by another	x / y	
+    %	Modulus	        Returns the division remainder	x % y	
+    ++	Increment	    Increases the value of a variable by 1	++x	
+    --	Decrement	    Decreases the value of a variable by 1	--x   */
+
+
+}
