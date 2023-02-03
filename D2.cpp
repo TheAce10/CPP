9ee9697141bdd0f0cf70cf95c6625dbcc2ad5c14
TheAce10 <krapahbless@gmail.com>
2023-02-03 00:30:19 +0000

    5

--- D2.cpp
+++ D2.cpp
@@ -27,4 +27,12 @@ int main() {
 using namespace std;
 
 int main() {
+    /*String Types
+    The string type is used to store a sequence of characters
+     (text). This is not a built-in type, but it behaves like 
+     one in its most basic usage. String values must be 
+     surrounded by double quotes:*/
+     string greeting = "Hunter x Hunter";
+    cout << greeting;
+
     
