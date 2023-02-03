c6b388fd624fc8b19a99af84d6a9d133cffd70dc
TheAce10 <krapahbless@gmail.com>
2023-02-03 00:29:11 +0000

    2

--- D2.cpp
+++ D2.cpp
@@ -6,7 +6,9 @@ int main() {
     std::cout << isit << "\n\n";
     std::cout << isnot;
 
-    
+    /*The char data type is used to store a single character.
+     The character must be surrounded by single quotes, like 'A' or 'c':*/
 
-   
-}
+    char grade = 'A';
+
+    
