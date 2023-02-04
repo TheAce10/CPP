f67b58c1138b6ab6ad736fc7faf6385cf39e0f99
TheAce10 <krapahbless@gmail.com>
2023-02-04 15:56:47 +0000

    C++ Break/Continue

--- D3.cpp
+++ D3.cpp
@@ -314,5 +314,53 @@ Statement 3 is executed (every time) after the code block has been executed.*/
 for (int c= 0;c<10;c++){
     cout<< c;
 }
+
+/*C++ Break
+You have already seen the break statement used in an earlier chapter of this tutorial. It was used to "jump out" of a switch statement.
+
+The break statement can also be used to jump out of a loop.
+
+This example jumps out of the loop when i is equal to 4:
+
+Example
+}*/
+for (int i = 0; i < 10; i++) {
+  if (i == 4) {
+    break;
+  }
+  cout << i << "\n";
+/*C++ Continue
+The continue statement breaks one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop.
+This example skips the value of 4:
+
+Example
+*/
+for (int i = 0; i < 10; i++) {
+  if (i == 4) {
+    continue;
+  }
+  cout << i << "\n";
+}
+// Break Example
+int i1 = 0;
+while (i1 < 10) {
+  cout << i1 << "\n";
+  i1++;
+  if (i1 == 4) {
+    break;
+  }
+}
+// Continue Example
+int i2 = 0;
+while (i2 < 10) {
+  if (i2 == 4) {
+    i2++;
+    continue;
+  }
+  cout << i2 << "\n";
+  i2++;
+}
+
+
     return 0;
 }
