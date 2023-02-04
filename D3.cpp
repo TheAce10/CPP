d3f97d3743cfb1be1361aaca52bf34c6c2d8dfc0
TheAce10 <krapahbless@gmail.com>
2023-02-04 15:31:04 +0000

    C+ switch case

--- D3.cpp
+++ D3.cpp
@@ -203,3 +203,60 @@ string var1= (x > y) ? "greater" : "lesser";
 
 return 0;
 }
+
+int main() {
+    /*C++ Switch Statements
+Use the switch statement to select one of many code blocks to be executed.
+
+Syntax
+switch(expression) {
+  case x:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+  default:
+    // code block
+}*/
+int day = 4;
+switch (day) {
+  case 1:
+    cout << "Monday";
+    break;
+  case 2:
+    cout << "Tuesday";
+    break;
+  case 3:
+    cout << "Wednesday";
+    break;
+  case 4:
+    cout << "Thursday";
+    break;
+  case 5:
+    cout << "Friday";
+    break;
+  case 6:
+    cout << "Saturday";
+    break;
+  case 7:
+    cout << "Sunday";
+    break;
+default:
+    cout << "day has to range 1 - 7";
+}
+// Outputs "Thursday" (day 4)
+/*The break Keyword
+When C++ reaches a break keyword, it breaks out of the switch 
+block.
+This will stop the execution of more code and case testing 
+inside the block.
+When a match is found, and the job is done, it's time for a 
+break. There is no need for more testing.
+The default Keyword
+The default keyword specifies some code to run if there is no 
+case match:*/
+
+
+    return 0;
+}
