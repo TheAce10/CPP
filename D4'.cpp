2bf32699c6e7a42131136262c97b987b69fac4b2
TheAce10 <krapahbless@gmail.com>
2023-02-05 23:29:05 +0000

    erro resolved

--- D4'.cpp
+++ D4'.cpp
@@ -68,30 +68,10 @@ struct myDataType { // This structure is named "myDataType"
 myDataType myVar;
 myVar.myNum= 145;
 myVar.myString= "George";
+return 0;
+}
 
 // Example
 // Use one structure to represent two cars:
 
 // Declare a structure named "car"
-struct car {
-  string brand;
-  string model;
-  int year;
-};
-  // Create a car structure and store it in myCar1;
-  car myCar1;
-  myCar1.brand = "BMW";
-  myCar1.model = "X5";
-  myCar1.year = 1999;
-
-  // Create another car structure and store it in myCar2;
-  car myCar2;
-  myCar2.brand = "Ford";
-  myCar2.model = "Mustang";
-  myCar2.year = 1969;
- 
-  // Print the structure members
-  cout << myCar1.brand << " " << myCar1.model << " " << myCar1.year << "\n";
-  cout << myCar2.brand << " " << myCar2.model << " " << myCar2.year << "\n";
-    return 0;
-}
