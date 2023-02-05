1ba92a44beaad12e240764f662dfb839b2977710
TheAce10 <krapahbless@gmail.com>
2023-02-05 23:11:55 +0000

    struct 2 error

--- D4'.cpp
+++ D4'.cpp
@@ -54,7 +54,44 @@ myCar2.year = 1969;
 cout << myCar1.brand << " " << myCar1.model << " " << myCar1.year << "\n";
 cout << myCar2.brand << " " << myCar2.model << " " << myCar2.year << "\n";
 
+// By giving a name to the structure, you can treat it as a data type. This means that you can create variables with this structure anywhere in the program at any time.
 
+// To create a named structure, put the name of the structure right after the struct keyword:
 
+struct myDataType { // This structure is named "myDataType"
+  int myNum;
+  string myString;
+};
+
+// To declare a variable that uses the structure, use the name of the structure as the data type of the variable:
+
+myDataType myVar;
+myVar.myNum= 145;
+myVar.myString= "George";
+
+// Example
+// Use one structure to represent two cars:
+
+// Declare a structure named "car"
+struct car {
+  string brand;
+  string model;
+  int year;
+};
+  // Create a car structure and store it in myCar1;
+  car myCar1;
+  myCar1.brand = "BMW";
+  myCar1.model = "X5";
+  myCar1.year = 1999;
+
+  // Create another car structure and store it in myCar2;
+  car myCar2;
+  myCar2.brand = "Ford";
+  myCar2.model = "Mustang";
+  myCar2.year = 1969;
+ 
+  // Print the structure members
+  cout << myCar1.brand << " " << myCar1.model << " " << myCar1.year << "\n";
+  cout << myCar2.brand << " " << myCar2.model << " " << myCar2.year << "\n";
     return 0;
 }
