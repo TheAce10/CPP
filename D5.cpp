82271c2376fde562b1650f578e177bf701b9292d
TheAce10 <krapahbless@gmail.com>
2023-02-06 23:30:47 +0000

    modify pointers

--- D5.cpp
+++ D5.cpp
@@ -120,6 +120,32 @@ cout << *ptr2 << "\n";
 /*Note that the * sign can be confusing here, as it does two different things in our code:
 
 When used in declaration (string* ptr), it creates a pointer variable.
-When not used in declaration, it act as a dereference operator.*/
+When not used in declaration, it act as a dereference operator.
+*/
+// Modify the Pointer Value
+// You can also change the pointer's value. But note that this 
+// will also change the value of the original variable:
+
+// Example
+string food = "Pizza";
+string* ptr = &food;
+
+// Output the value of food (Pizza)
+cout << food << "\n";
+
+// Output the memory address of food (0x6dfed4)
+cout << &food << "\n";
+
+// Access the memory address of food and output its value (Pizza)
+cout << *ptr << "\n";
+
+// Change the value of the pointer
+*ptr = "Hamburger";
+
+// Output the new value of the pointer (Hamburger)
+cout << *ptr << "\n";
+
+// Output the new value of the food variable (Hamburger)
+cout << food << "\n";
     return 0;
 }   
