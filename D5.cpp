1276ff54cfed720d0bc570ac01b10c1a539cc715
TheAce10 <krapahbless@gmail.com>
2023-02-06 09:35:28 +0000

    Day 5, class excercise

---
+++ D5.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+    //BMI CALC
+    float height,weight,BMI;
+    cout << "BMI cALCULATION\n" << "Enter your height(cm)\t";
+    cin >> height ;
+    cout << "Enter your weight(kg)\n";
+    cin >>weight;
+    cout << "Height:\t"<<height<<"\nWeight:\t"<<weight;
+
+    // convert height to meters
+    height/=100;
+
+    // BMI
+    BMI= weight/(height*height);
+    cout << "Calcuated BMI:\t"<< BMI;
+    // Check for BMI Feedback
+    if (BMI<18.5){
+        cout << "\n You are underweight";
+    }
+    else if(18.5<BMI<24.9){
+        cout << "\n Your weight is normal";
+    }
+    else if(25<BMI<29.9){
+        cout << "\n You are overweight";
+    }
+    else if(30<BMI<39.9){
+        cout << "\n Your are obese";
+    }
+    else if(BMI>=40){
+        cout << "\n Your morbidly obese";
+    }
+    return 0;
+}
