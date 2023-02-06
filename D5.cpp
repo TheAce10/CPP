ae53fa612a316d581b4f1bfff4e1a718c86b5161
TheAce10 <krapahbless@gmail.com>
2023-02-06 23:29:03 +0000

    deferencing

--- D5.cpp
+++ D5.cpp
@@ -104,5 +104,22 @@ Tip: There are three ways to declare pointer variables, but the first way is pre
 string* mystring; // Preferred
 string *mystring;
 string * mystring;*/
+
+/*Get Memory Address and Value
+In the example from the previous page, we used the pointer variable to get the memory address of a variable (used together with the & reference operator). However, you can also use the pointer to get the value of the variable, by using the * operator (the dereference operator):
+
+Example*/
+string food2 = "Pizza";  // Variable declaration
+string* ptr2 = &food2;    // Pointer declaration
+
+// Reference: Output the memory address of food with the pointer (0x6dfed4)
+cout << ptr2 << "\n";
+
+// Dereference: Output the value of food with the pointer (Pizza)
+cout << *ptr2 << "\n";
+/*Note that the * sign can be confusing here, as it does two different things in our code:
+
+When used in declaration (string* ptr), it creates a pointer variable.
+When not used in declaration, it act as a dereference operator.*/
     return 0;
 }   
