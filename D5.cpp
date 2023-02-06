c69133c0412e5fd93b59ffee583e4155f33bf16c
TheAce10 <krapahbless@gmail.com>
2023-02-06 22:22:24 +0000

    References

--- D5.cpp
+++ D5.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 using namespace std;
+/*
 int main() {
     //BMI CALC
     float height,weight,BMI;
@@ -35,7 +36,7 @@ int main() {
     }
     return 0;
 }
-
+*/
 int main() {
     string acd= "bread";
     string &newdata= acd;
@@ -46,5 +47,25 @@ int main() {
     string &meal = food;    // reference to food
     cout << food << "\n";  // Outputs Pizza
     cout << meal << "\n";  // Outputs Pizza
+
+    /*Memory Address
+In the example from the previous page, the & operator was used to create a reference variable. But it can also be used to get the memory address of a variable; which is the location of where the variable is stored on the computer.
+
+When a variable is created in C++, a memory address is assigned to the variable. And when we assign a value to the variable, it is stored in this memory address.
+
+To access it, use the & operator, and the result will represent where the variable is stored:
+
+Example
+*/
+cout << &food; // Outputs address e.g. 0x5e7c3ff9c0
+
+// Note: The memory address is in hexadecimal form (0x..).
+/*And why is it useful to know the memory address?
+References and Pointers (which you will learn about in the next chapter) are important in C++, because they give you the ability to manipulate the data in the computer's memory - which can reduce the code and improve the performance.
+
+These two features are one of the things that make C++ stand 
+out from other programming languages, like Python and Java.*/
+
+
     return 0;
 }
