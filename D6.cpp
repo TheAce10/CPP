f332f5a61d7baa3f4133e80b0768f7a186beed62
TheAce10 <krapahbless@gmail.com>
2023-02-07 22:50:30 +0000

    return keyword explained

--- D6.cpp
+++ D6.cpp
@@ -116,6 +116,47 @@ Note that when you are working with multiple parameters,
 the function call must have the same number of arguments as 
 there are parameters, and the arguments must be passed in the 
 same order.*/
+
+/*Return Values
+The void keyword, used in the previous examples, indicates that the function should not return a value. If you want the function to return a value, you can use a data type (such as int, string, etc.) instead of void, and use the return keyword inside the function:
+
+Example
+int myFunction(int x) {
+  return 5 + x;
+}
+
+int main() {
+  cout << myFunction(3);
+  return 0;
+}
+
+// Outputs 8 (5 + 3)
+This example returns the sum of a function with two parameters:
+
+Example
+int myFunction(int x, int y) {
+  return x + y;
+}
+
+int main() {
+  cout << myFunction(5, 3);
+  return 0;
+}
+
+// Outputs 8 (5 + 3)
+You can also store the result in a variable:
+
+Example
+int myFunction(int x, int y) {
+  return x + y;
+}
+
+int main() {
+  int z = myFunction(5, 3);
+  cout << z;
+  return 0;
+}
+// Outputs 8 (5 + 3)*/
     return 0;
 }
 // definition
