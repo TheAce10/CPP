31c7f1b063893927cfc6cb212f30ee4100e54184
TheAce10 <krapahbless@gmail.com>
2023-02-07 23:26:43 +0000

    Pass Arrays as Function Parameters

--- D6'.cpp
+++ D6'.cpp
@@ -6,7 +6,7 @@ void swapNums(int &x, int &y) {
   x = y;
   y = z;
 }
-
+void myFunction(int myNumbers[5]);
 int main() {
     /*Pass By Reference
 In the examples from the previous page, we used normal variables when we passed parameters to a function. You can also pass a reference to the function. This can be useful when you need to change the value of the arguments:
@@ -24,5 +24,20 @@ int firstNum = 10;
 
   cout << "After swap: " << "\n";
   cout << firstNum << secondNum << "\n";
+
+  int myNumbers[5] = {10, 20, 30, 40, 50};
+  myFunction(myNumbers);
     return 0;
+}
+
+void myFunction(int myNumbers[5]) {
+  for (int i = 0; i < 5; i++) {
+    cout << myNumbers[i] << "\n";
+    /*xample Explained
+The function (myFunction) takes an array as its parameter (int myNumbers[5]), and loops through the array elements with the for loop.
+
+When the function is called inside main(), we pass along the myNumbers array, which outputs the array elements.
+
+Note that when you call the function, you only need to use the name of the array when passing it as an argument myFunction(myNumbers). However, the full declaration of the array is needed in the function parameter (int myNumbers[5]).*/
+  }
 }
