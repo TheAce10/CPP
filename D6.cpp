ebd6421b850ce0bbe3ba286bfdc772dc42904201
TheAce10 <krapahbless@gmail.com>
2023-02-07 22:41:52 +0000

    function parameters

--- D6.cpp
+++ D6.cpp
@@ -40,6 +40,34 @@ C++ provides some pre-defined functions, such as main(), which is used to execut
 
 To create (often referred to as declare) a function, specify the name of the function, followed by parentheses ():*/
     numerical();
+
+/*Parameters and Arguments
+Information can be passed to functions as a parameter. Parameters act as variables inside the function.
+
+Parameters are specified after the function name, inside the parentheses. You can add as many parameters as you want, just separate them with a comma:
+
+Syntax
+void functionName(parameter1, parameter2, parameter3) {
+  // code to be executed
+}*/
+
+/*The following example has a function that takes a string called fname as parameter. When the function is called, we pass along a first name, which is used inside the function to print the full name:
+
+Example
+void myFunction(string fname) {
+  cout << fname << " Refsnes\n";
+}
+
+int main() {
+  myFunction("Liam");
+  myFunction("Jenny");
+  myFunction("Anja");
+  return 0;
+}
+
+// Liam Refsnes
+// Jenny Refsnes
+// Anja Refsnes*/
     return 0;
 }
 // definition
