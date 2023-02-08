ad9276f35deb71ba92cf9b2446e5ea6a74774e6c
TheAce10 <krapahbless@gmail.com>
2023-02-08 15:37:19 +0000

    1

--- D8.cpp
+++ D8.cpp
@@ -26,29 +26,3 @@ using namespace std;
 // cout << "Your height is"<<feet<<"ft"<<inches <<"inches";
 // }
 
-int main () {
-    // float a;
-    // cout << "Enter height(cm)\t";
-    // cin >> a;
-    // convert(a);
-int aa,seq=0,j=0;
-cout << "Fibonacci sequence\n Enter length for sequence\t";
-cin >>aa;
-for(int i=0;i<=aa;i++){
-    if (i=0){
-        seq= 0;
-    }
-    else if (i=1){
-        seq= 1;
-    }
-    else if (i>1){
-    seq+=seq;
-    }
-    else {
-    cout<< "You entered a negative number";
-    break;
-    };
-    }
-    cout << "Fibonacci sequence to 5 is " << seq;
-return 0;
-}
---
+++ D8/1.cpp
@@ -0,0 +1,26 @@
+int main () {
+    // float a;
+    // cout << "Enter height(cm)\t";
+    // cin >> a;
+    // convert(a);
+int aa,seq=0,j=0;
+cout << "Fibonacci sequence\n Enter length for sequence\t";
+cin >>aa;
+for(int i=0;i<=aa;i++){
+    if (i=0){
+        seq= 0;
+    }
+    else if (i=1){
+        seq= 1;
+    }
+    else if (i>1){
+    seq+=seq;
+    }
+    else {
+    cout<< "You entered a negative number";
+    break;
+    };
+    }
+    cout << "Fibonacci sequence to 5 is " << seq;
+return 0;
+}
