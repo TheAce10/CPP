b18011c379966681c1b703f181e32721de66bda2
TheAce10 <krapahbless@gmail.com>
2023-02-08 09:42:47 +0000

    convert to feet/inches

--- D8.cpp
+++ D8.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 #include <string>
 
-
-long int sum_natural(int n){
-    int sum1= 0;
-    for (int i=1; i < n; i++){
-        sum1+= sum1++;
-    }
-    return sum1;
-}
+// Question 1
+// long int sum_natural(int n){
+//     int sum1= 0;
+//     for (int i=1; i < n; i++){
+//         sum1+= sum1++; //sum1+= i
+//     }
+//     return sum1;
+// }
 using namespace std;
-int main() {
-    int a;
-    cout <<"Sum of Natural numbers to the nth term\n" << "Enter a nth term for your sum: \t" ;
+// int main() {
+//     int a;
+//     cout <<"Sum of Natural numbers to the nth term\n" << "Enter a nth term for your sum: \t" ;
+//     cin >> a;
+//     cout << "Sum of natural numbers up to"<<a << ":"<<sum_natural(a);
+//     return 0;
+// }
+
+void convert(float n){
+    float inches, feet;
+inches = n/2.5;
+feet= inches % 12;
+inches= (inches/12)-feet
+cout << "Your height is"<<feet<<"ft"<<inches <<"inches";
+}
+
+int main () {
+    float a;
+    cout << "Enter height(cm)\t";
     cin >> a;
-    cout << "Sum of natural numbers to the "<<a<<"term is: "<<sum_natural(a);
-    return 0;
+    convert(a);
+
 }
