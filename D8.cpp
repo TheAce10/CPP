bfff168040dd99e86f6c0542d50329ff60754beb
TheAce10 <krapahbless@gmail.com>
2023-02-08 09:58:05 +0000

    fibonacci

--- D8.cpp
+++ D8.cpp
@@ -18,18 +18,36 @@ using namespace std;
 //     return 0;
 // }
 
-void convert(float n){
-    float inches, feet;
-inches = n/2.5;
-feet= inches % 12;
-inches= (inches/12)-feet
-cout << "Your height is"<<feet<<"ft"<<inches <<"inches";
-}
+// void convert(float n){
+//     float inches, feet;
+// inches = n/2.5;
+// feet= inches % 12;
+// inches= (inches/12)-feet;
+// cout << "Your height is"<<feet<<"ft"<<inches <<"inches";
+// }
 
 int main () {
-    float a;
-    cout << "Enter height(cm)\t";
-    cin >> a;
-    convert(a);
-
+    // float a;
+    // cout << "Enter height(cm)\t";
+    // cin >> a;
+    // convert(a);
+int aa,seq=0,j=0;
+cout << "Fibonacci sequence\n Enter length for sequence\t";
+    cin >>aa;
+for(int i=0;i<=aa;i++){
+    if (i=0){
+        seq= 0;
+    }
+    else if (i=1){
+        seq= 1;
+    }
+    else if (i>1){
+    seq+=seq;
+    }
+    else {
+cout<< "You entered a negative number";
+    };
+    }
+cout << "Fibonacci sequence to 5 is " << seq;
+return 0;
 }
