970d510cc75a7e7690a1c7a2843e6d7ab5cdfe7f
TheAce10 <krapahbless@gmail.com>
2023-02-09 16:25:52 +0000

    1

---
+++ D9/1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void swapNums(int &x, int &y) {
+  int z = x;
+  x = y;
+  y = z;
+}
+
+int main() {
+    int a=1,b=2;
+    cout << "a is " <<a;
+    cout << "b is " <<b;
+    swapNums(a,b);
+    cout << "a is " <<a;
+    cout << "b is " <<b;
+
+    swapNums1(a,b);
+    cout << "a is " <<a;
+    cout << "b is " <<b;
+
+    return 0;
+}
