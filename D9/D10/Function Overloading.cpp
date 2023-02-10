833f4765402960996dd6da3b9fbe2e19b3cc2baa
TheAce10 <krapahbless@gmail.com>
2023-02-10 16:38:08 +0000

    3

--- D9/D10/Function Overloading.cpp
+++ D9/D10/Function Overloading.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 
 using namespace std;
-int myFunction(int x)
-float myFunction(float x)
-double myFunction(double x, double y)
+int myFunction(int x);
+float myFunction(float x);
+double myFunction(double x, double y);
 
+int plusFuncInt(int x, int y) {
+  return x + y;
+}
+
+double plusFuncDouble(double x, double y) {
+  return x + y;
+}
 int main() {
 
     return 0;
