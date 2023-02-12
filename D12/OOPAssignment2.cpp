2061a00134a24db9c7440862ffab9aefebc4ef5e
TheAce10 <krapahbless@gmail.com>
2023-02-12 15:51:03 +0000

    1

---
+++ D12/OOPAssignment2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string a,b,c,d;
+    for(int i=0;i<3;i++){
+    if(i=0 ){
+    cin >>a;
+    cout <<a<< ":";
+    continue;}
+    else if (i=1)
+    {
+    cin >>b;
+    cout <<a<<":"<<b<< ":";
+    continue;
+    }
+    else {
+    cin >>c;
+    break;}
+    
+    }
+    cout << a<<":"<<b<<":"<<c;
+    return 0;
+}
