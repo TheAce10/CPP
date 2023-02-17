7b1199e702f1cf94d60936df7f81d8c652537c4d
TheAce10 <krapahbless@gmail.com>
2023-02-17 23:22:51 +0000

    4

--- D12/Assignment.cpp
+++ D12/Assignment.cpp
@@ -16,7 +16,7 @@ int main() {
             break;
         }
         long total_seconds = hms_to_secs(hours, minutes, seconds);
-        cout <<"h"<< "is equivalent to : " << total_seconds << "sec(s)"<<endl;
+        cout <<"\n"<<hrs<<"h:"<<min<< "m:"<<sec<<"s"<<"<<"h"<< "is equivalent to : " << total_seconds << "sec(s)"<<endl;
     }
     return 0;
 }
