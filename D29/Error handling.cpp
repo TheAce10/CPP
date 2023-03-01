58428ad13c2581bbb93602c00c0e1bbc3a5c9cdd
TheAce10 <krapahbless@gmail.com>
2023-03-01 20:02:04 +0000

    2

---
+++ D29/Error handling.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+int main(){
+    /*C++ try and catch
+Exception handling in C++ consist of three keywords: try, throw and catch:
+
+The try statement allows you to define a block of code to be tested for errors while it is being executed.
+
+The throw keyword throws an exception when a problem is detected, which lets us create a custom error.
+
+The catch statement allows you to define a block of code to be executed, if an error occurs in the try block.
+
+The try and catch keywords come in pairs:
+
+Example*/
+
+}
