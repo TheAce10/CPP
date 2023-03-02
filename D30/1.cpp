c26f28040e4ea5e1a01be9f8227ca30297af7588
TheAce10 <krapahbless@gmail.com>
2023-03-02 11:05:20 +0000

    3

--- D30/1.cpp
+++ D30/1.cpp
@@ -4,3 +4,14 @@ struct {             // Structure declaration
   int myNum;         // Member (int variable)
   string myString;   // Member (string variable)
 } myStructure;       // Structure variable
+
+int main(){
+  string food = "Pizza";
+string &meal = food;
+
+cout << food << "\n";  // Outputs Pizza
+cout << meal << "\n";  // Outputs Pizza
+cout << &food << "\n";
+cout << &meal << "\n";
+return 0;
+}
