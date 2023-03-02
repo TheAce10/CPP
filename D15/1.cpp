bee32bb55a9f171a8f905f0426b8dd2c3ef6b94c
TheAce10 <krapahbless@gmail.com>
2023-03-02 17:08:08 +0000

    2

--- D15/1.cpp
+++ D15/1.cpp
@@ -2,10 +2,19 @@
 
 using namespace std;
 
+//Classes
+class 
+{
+private:
+    /* data */
+public:
+    (/* args */) {}
+    ~() {}
+};
 
 
 int main() {
     cout << "Back at it";
-    
+
     return 0;
 }
