1b934049b396f7357beb9d7760c687cc3ca2bce0
TheAce10 <krapahbless@gmail.com>
2023-03-02 11:58:48 +0000

    7

--- D30/zeroSmaller.cpp
+++ D30/zeroSmaller.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 void zeroSmaller(int, int);
-
+int main(){
+    int z,y;
+    cin >> z ;
+    cin >> y;
+    zeroSmaller(z,y);
+}
 
 void zeroSmaller(int a, int b){
     (a>b)?a=0:b=0;
