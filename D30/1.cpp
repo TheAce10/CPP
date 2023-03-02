f122b53a75b17cf2f415f803756e154a479eaec8
TheAce10 <krapahbless@gmail.com>
2023-03-02 10:58:22 +0000

    2

---
+++ D30/1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+struct {             // Structure declaration
+  int myNum;         // Member (int variable)
+  string myString;   // Member (string variable)
+} myStructure;       // Structure variable
