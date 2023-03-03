65046878fb9cb75df2f74cd1d68f770415c2659e
TheAce10 <krapahbless@gmail.com>
2023-03-03 18:16:47 +0000

    4

--- D31/files.cpp
+++ D31/files.cpp
@@ -3,22 +3,5 @@
 
 using namespace std;
 int main(){
-    string text;
-    ofstream prog("progress.txt");
-    prog << "Level = 3\n123456789";
-    prog.close();
-    ifstream prog1("progress.txt");
-    while (getline (prog1, text)){
-        cout << text.length();
-        cout << "\n";
-    }
-    prog1.close();
-
-    /*write a c++ program file*/
-    ofstream cplus("1.cpp");
-    cplus << "#include <iostream>\n#include <fstream>\nusing namespace std;\n\nint main() {"
-    <<"\nofstream new1(\"new.txt\");\nnew1 << \"i did it\"; \nnew1.close();\nreturn 0;\n}";
-    cplus.close();
-    /*a program that creates another program*/
 
 }
