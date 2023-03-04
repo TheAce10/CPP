8e5f862d3e3b4638c4457b0fa23df16bc70a3f87
TheAce10 <krapahbless@gmail.com>
2023-03-04 17:52:55 +0000

    2

--- D32/1.cpp
+++ D32/1.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <fstream>
+/*C++ Files
+The fstream library allows us to work with files.
+To use the fstream library, include both the standard <iostream> AND the <fstream> header file:*/
+
 using namespace std;
 
 int main() {
+    int vase;
+    /*There are three classes included in the fstream library, which are used to create, write or read files:
+
+Class	Description
+ofstream	Creates and writes to files
+ifstream	Reads from files
+fstream	    A combination of ofstream and ifstream: creates, reads, and writes to files */
+    // Create and open a text file
+  ofstream MyFile("filename.txt");
+
+  
     return 0;
 }
