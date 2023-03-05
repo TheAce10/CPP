585983d215448c14592358b13f559717a4aa11e5
TheAce10 <krapahbless@gmail.com>
2023-03-05 16:43:18 +0000

    2

--- D33/polymorphism.cpp
+++ D33/polymorphism.cpp
@@ -6,3 +6,11 @@ Polymorphism means "many forms", and it occurs when we have many classes that ar
 Like we specified in the previous chapter; Inheritance lets us inherit attributes and methods from another class. Polymorphism uses those methods to perform different tasks. This allows us to perform a single action in different ways.
 
 For example, think of a base class called Animal that has a method called animalSound(). Derived classes of Animals could be Pigs, Cats, Dogs, Birds - And they also have their own implementation of an animal sound (the pig oinks, and the cat meows, etc.):*/
+
+// Base class
+class Animal {
+  public:
+    void animalSound() {
+    cout << "The animal makes a sound \n" ;
+  }
+};
