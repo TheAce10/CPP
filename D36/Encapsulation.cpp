2928aa87e4f1938e57d0f3c4dd6fa707c6f607ed
TheAce10 <krapahbless@gmail.com>
2023-03-06 10:47:42 +0000

    7

--- D36/Encapsulation.cpp
+++ D36/Encapsulation.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+using namespace std;
 /*Encapsulation
 The meaning of Encapsulation, is to make sure that "sensitive" data is hidden from users. To achieve this, you must declare class variables/attributes as private (cannot be accessed from outside the class). If you want others to read or modify the value of a private member, you can provide public get and set methods.
 
 */
+
+/*Access Private Members
+To access a private attribute, use public "get" and "set" methods:
+
+Example*/
+
+
