ba7949e530ca4999cacd9ec1cd06a3c5edfaf1c3
TheAce10 <krapahbless@gmail.com>
2023-03-06 10:48:39 +0000

    10

--- D36/Encapsulation.cpp
+++ D36/Encapsulation.cpp
@@ -21,3 +21,15 @@ class Employee {
     void setSalary(int s) {
       salary = s;
     }
+    // Getter
+    int getSalary() {
+      return salary;
+    }
+};
+
+int main() {
+  Employee myObj;
+  myObj.setSalary(50000);
+  cout << myObj.getSalary();
+  return 0;
+}
