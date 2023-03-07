e591805491f140be37b76323efd49261d50ba58f
TheAce10 <krapahbless@gmail.com>
2023-03-07 11:28:52 +0000

    5

--- D35/1.cpp
+++ D35/1.cpp
@@ -23,3 +23,6 @@ class Car: public vehicle {
     /*Why And When To Use "Inheritance"?
 - It is useful for code reusability: reuse attributes and methods of an existing class when you create a new class.*/
     };
+
+// Multilevel inheritance
+class sportsCar : public Car{
