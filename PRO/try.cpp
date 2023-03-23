13256737789273cec6c9d6b52679324c6fbc2633
TheAce10 <krapahbless@gmail.com>
2023-03-23 12:11:33 +0000

    3

--- PRO/try.cpp
+++ PRO/try.cpp
@@ -68,3 +68,55 @@ public:
     outcomeTree(string rootData) {
         root = new outcomeNode(rootData);
     }
+    void insert(string outcome1, string outcome2, outcomeNode* node) {
+        node->left = new outcomeNode(outcome1);
+        node->right = new outcomeNode(outcome2);
+    }
+    string play(int choice) {
+        outcomeNode* current = root;
+        while (current != nullptr) {
+            cout << current->data << endl;
+            if (choice == 1) {
+                current = current->left;
+            }
+            else if (choice == 2) {
+                current = current->right;
+            }
+            else {
+                return "Invalid choice";
+            }
+        }
+        return current->data;
+    }
+};
+
+int main() {
+    outcomeTree tree("You are at a crossroads. Do you go left or right?");
+    tree.insert("You go left and find a treasure!", "You go right and get lost.", tree.root);
+    tree.insert("You go left and find a dragon!", "You go right and find a magic sword.", tree.root->left);
+    tree.insert("You go left and find a dragon!", "You go right and find a magic sword.", tree.root->left->left);
+    tree.insert("You go left and find a dragon!", "You go right and find a magic sword.", tree.root->left->right);
+    tree.insert("You go left and find a dragon!", "You go right and find a magic sword.", tree.root->right);
+    tree.insert("You go left and find a dragon!", "You go right and find a magic sword.", tree.root->right->left);
+    tree.insert("You go left and find a dragon!", "You go right and find a magic sword.", tree.root->right->right);
+
+
+    
+    cout << "Choose 1 or 2:" << endl;
+    int choice;
+    cin >> choice;
+    cout << tree.play(choice) << endl;
+    return 0;
+}
+/*In this example, we define a outcomeNode class to represent each node in the binary tree, with a data member variable to store the outcome as a string, and left and right pointers to outcomeNode objects for the left and right children.
+
+We then define an outcomeTree class to represent the binary tree itself, with a root pointer to the outcomeNode object at the root of the tree. The outcomeTree class will have member functions to insert new outcomes into the tree and play the game by traversing the tree based on the player's choices.
+
+In the insert function, we take two new outcomes as strings and a outcomeNode object as input, and create new outcomeNode objects for the left and right children with the new outcomes as their data.
+
+In the play function, we take the player's choice as an integer input and traverse the tree accordingly. We print out the current outcome at each node, and return the final outcome at the leaf node based on the player's choices.
+
+In the main function, we create a new outcomeTree object with the root outcome as a string, and insert two new outcomes for each child node. We then prompt the player to make a choice and call the play function to start the game, printing out the final outcome.
+*/
+
+
